25eb54cad0439469fa372eed911c468eafcb3b13
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-03-16 21:18:29 +0700

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,37 +1,21 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 import pytest
 
+
+
+@pytest.mark.usefixtures('initialize_browser_state', 'chromium_page_with_state')
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+def test_empty_courses_list(initialize_browser_state: Page, chromium_page_with_state: Page):
+        initialize_browser_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        registration_email_input.fill('user.name@gmail.com')
+        courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+        courses_results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+        courses_description_results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+        #dashboard_title = chromium_page_with_state.get_by_test_id('dashboard-toolbar-title-text')
 
-        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        registration_username_input.fill('username')
-
-        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        registration_password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-
-        context.storage_state(path='browser-state.json')
-
-        context = browser.new_context(storage_state="browser-state.json")
-        page = context.new_page()
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        courses_results = page.get_by_test_id('courses-list-empty-view-title-text')
-
+        #expect(dashboard_title).to_be_visible()
         expect(courses_title).to_be_visible()
         expect(courses_results).to_be_visible()
+        expect(courses_description_results).to_be_visible()
